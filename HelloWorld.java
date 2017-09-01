3165d2ce57f93ed25653ca7882a9e27975ad5733
cjolley787 <31425755+cjolley787@users.noreply.github.com>
2017-09-01 10:06:31 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Connor Jolley
+ * Date: 9/1/2017
+ * 
+ * A program to print my name
+ * @author cjolley787
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Connor Jolley");
+		
+	}
+}
