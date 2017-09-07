81c8e1cba804599dbbeef766eddcb88134824f65
shengjlim <sheng-jie.lim@huskers.unl.edu>
2017-09-07 15:00:10 -0500

    I added my name booooiiiiiigit add -A!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 /**
- * Author: Connor Jolley and Sheng-Jie Lim
+ * Author: Connor Jolley and its yah boi shengjeezy lim muddafugga
  * Date: 9/1/2017
- * 
+ *
  * A program to print my name
  * @author cjolley787
  *
@@ -9,8 +9,8 @@
 public class HelloWorld {
 
 	public static void main(String args[]) {
-		
+
 		System.out.println("Connor Jolley and Sheng-Jie Lim");
-		
+
 	}
 }
